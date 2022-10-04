59221f340407b69adbc47131a34b0509410f516b
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2022-10-04 08:47:25 +0200

    chat

---
+++ ChatSystem/ChatSystem/ChatSystem/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct2D1;
+using System.Drawing;
+using Color = Microsoft.Xna.Framework.Color;
+using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
+
+namespace ChatSystem
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        PlayerInput playerInput;
+
+        private Vector2 screenSize;
+
+        public Vector2 ScreenSize { get => screenSize; set => screenSize = value; }
+
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 900;
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            ScreenSize = new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            playerInput = new PlayerInput(Content);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            playerInput.LoadContent();
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            playerInput.handleOverallInput();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            playerInput.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ChatSystem/ChatSystem/ChatSystem/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new ChatSystem.Game1();
+game.Run();
